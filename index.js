7836f825fd1367ec4cfdb62160526970bc3cedc9
Aurelien Gasser <aurelien.gasser@gmail.com>
2016-09-26 00:42:47 +0200

    server working

---
+++ index.js
@@ -0,0 +1,26 @@
+var express, app, http, io;
+setupServer();
+
+function setupServer() {
+   express = require('express');
+   app = express();
+   http = require('http').Server(app);
+   io = require('socket.io')(http);
+   io.set('transports', ['websocket']);
+
+   app.use(express.static('public'));
+
+   http.listen(process.env.PORT || 3000, function () {
+     console.log('listening on *:3000');
+   });
+
+   io.on('connection', function (_socket) {
+     socket = _socket;
+     pokeio.socket = socket;
+     console.log('User connected');
+     
+     socket.on('disconnect', function () {
+       console.log(' User disconnected');
+     });
+   });
+}
