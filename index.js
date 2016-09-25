3e764ed9b673abee297ba0b1a91c93bb19eef5c5
Aurelien Gasser <aurelien.gasser@gmail.com>
2016-09-26 01:21:03 +0200

    init tiles + player name

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
+var mapHeight = 5;
+var mapWidth = 10;
+
 var express, app, http, io;
 var sockets = [];
 var icons;
+var tiles = [];
 
 readIcons(function(err, _icons) {
   if (err) return;
   icons = _icons;
-  setupServer();
+  initTiles();
+  setupExpress();
 });
 
-function setupServer() {
+function initTiles() {
+  var numIconsToUse = mapWidth * mapHeight / 2;
+  var iconsToUse = [];
+  for (var k = 0; k < numIconsToUse; ++k) {
+    iconsToUse.push([getRandomIcon(), 2]);
+  }
+  
+  for (var i = 0; i < mapWidth; ++i) {
+    for (var j = 0; j < mapHeight; ++j) {
+      var idx = Math.floor(Math.random() * Object.keys(iconsToUse).length);
+      var o = iconsToUse[idx];
+      if (--o[1] == 0) {
+        iconsToUse.splice(idx, 1);
+      }
+      if (!tiles[i]) {
+        tiles[i] = [];
+      }
+      tiles[i].push(o[0]);
+    }
+  }
+}
+
+function getRandomIcon() {
+  var idx = Math.floor(Math.random * icons.length);
+  return icons.splice(idx, 1)[0];
+}
+
+function setupExpress() {
    express = require('express');
    app = express();
    http = require('http').Server(app);
@@ -23,7 +55,13 @@ function setupServer() {
 
    io.on('connection', function (socket) {
      sockets.push(socket);
+     socket.playerName = 'Player ' + Math.foor(Math.random() * 10000);
      console.log('User connected');
+     sendGameState(socket);
+     
+     socket.on('send-info', function(data) {
+       socket.playerName = data.name;
+     })
      
      socket.on('disconnect', function () {
        var idx = sockets.indexOf(socket);
@@ -35,10 +73,15 @@ function setupServer() {
 
 function broadcast() {
   for (var i = 0; i < sockets.length; ++i) {
-    socket.emit('game_state', {
-      numPlayers: sockets.length
-    })
+    sendGameState(sockets[i]);
   };
+}
+
+function sendGameState(socket) {
+  socket.emit('game-state', {
+    numPlayers: sockets.length,
+    players: sockets.map(function(s) { return { name: s.playerName }})
+  });  
 }
 
 function readIcons(cb) {
