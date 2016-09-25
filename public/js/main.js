2356bccd1f0f84176b0e84b727390646ff9f8f80
Pouya <pouyajoon@gmail.com>
2016-09-26 01:52:31 +0200

    lint

--- public/js/main.js
+++ public/js/main.js
@@ -4,31 +4,38 @@
 
   var socket, app, game, Game, Card;
 
-
-  Game = function(socket, gameState) {
+  Game = function(socket) {
     this.socket = socket;
-    this.gameState = gameState || {
-      board: []
-    };
-    this.initList();
+    this.cards = null;
   };
 
-  Game.prototype.initList = function() {
+  Game.prototype.updateBoard = function() {
     var i;
-    for (i = 0; i < this.gameState.board.length; i += 1) {
-      this.gameState.board.push(new Card(socket, {
-        id: i,
-        icon: null,
-        player: null
-      }));
+    if (this.cards === null) {
+      this.cards = {};
+      for (i = 0; i < this.gameState.board.length; i += 1) {
+        this.cards[i] = new Card(this, {
+          id: i,
+          icon: null,
+          player: null
+        });
+      }
+    } else {
+      for (i = 0; i < this.gameState.board.length; i += 1) {
+        this.cards[i].data = this.gameState.board[i];
+      }
     }
+    console.log(this.cards);
   };
-  Game.prototype.setGameState = function() {
-    var count = Math.sqrt(game.gameState.board.length);
+
+  Game.prototype.setGameState = function(gameState) {
+    var count = Math.sqrt(gameState.board.length);
     game.ui = {
       percentage: 100 / count,
       widthContainer: 100 * count
     };
+    this.gameState = gameState;
+    this.updateBoard();
   };
 
   socket = io({
@@ -39,16 +46,7 @@
 
   game = new Game(socket);
 
-  socket.on('connect', function() {
-    console.log('connected');
-    socket.emit('send-info', {
-      'name': 'pouya'
-    });
-    socket.on('game-state', function(gameState) {
-      game.gameState = gameState;
-      console.log('gameState', gameState);
-    });
-  });
+
 
   Card = function(socket, data) {
     this.socket = socket;
@@ -65,9 +63,29 @@
   };
 
   app = angular.module('myApp', []);
-  app.controller('MainController', function($scope, $http, $rootScope) {
+  app.controller('MainController', function($scope) {
     game.$s = $scope;
     $scope.game = game;
+
+    var $on = function(key, callback) {
+      socket.on(key, function(res) {
+        $scope.$apply(function() {
+          return callback(res);
+        });
+      });
+    };
+
+    socket.on('connect', function() {
+      console.log('connected');
+      socket.emit('send-info', {
+        'name': 'pouya'
+      });
+      $on('game-state', function(gameState) {
+        game.setGameState(gameState);
+        console.log('gameState', gameState);
+      });
+    });
+
   });
 
 
